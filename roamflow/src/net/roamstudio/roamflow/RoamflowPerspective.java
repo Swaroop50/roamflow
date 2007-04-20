7c0391f6800d00e49a30ecfd5de0cbd41bfd1f25
kite.china <kite.china@2115dfd6-772e-0410-8346-75592170a990>
2007-04-20 13:28:18 +0000

    初始导入。

---
+++ roamflow/src/net/roamstudio/roamflow/RoamflowPerspective.java
@@ -0,0 +1,10 @@
+package net.roamstudio.roamflow;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class RoamflowPerspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
